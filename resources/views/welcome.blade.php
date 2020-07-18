0f114ba3bb744dac2151217d1664e97d13f32ca9
Sutthinan Kantala <momona671@gmail.com>
2020-07-18 11:46:58 +0700

    ใหม่

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Flame
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+});
+
+Route::get('/user/{name}', function ($name) {
+    return "สวัสดี $name";
+
+});
+Route::get('/บวกเลข/{num1}/{num2}', function ($num1,$num2) {
+    return $num1 + $num2 ;
+
 });
